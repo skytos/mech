72180a84823aefdb262e8b8f35959b380653c37e
Skylar Broad <skylar.broad@gmail.com>
2021-01-28 14:24:00 -0800

    we have many hexes

--- Assets/Models/HexMap.cs
+++ Assets/Models/HexMap.cs
@@ -7,12 +7,23 @@ public class HexMap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateMap();
     }
 
+    public GameObject HexPrefab;
+
     // Update is called once per frame
-    void Update()
+    public void GenerateMap()
     {
-        
+        for (int column = 0; column < 10; column++) {
+            for (int row = 0; row < 10; row++) {
+                Instantiate(
+                    HexPrefab,
+                    new Vector3(column, 0, row),
+                    Quaternion.identity,
+                    this.transform
+                );
+            }
+        }
     }
 }
